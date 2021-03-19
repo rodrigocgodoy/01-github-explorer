686e0aea7cc52a8a85f4c15d13f3e3b1de4b5b98
Rodrigo Godoy MacBook <rodrigocgodoy@hotmail.com>
2021-03-18 22:31:15 -0300

    Modify all pages of typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/respositories.scss';
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -12,14 +18,12 @@ export function RepositoryList() {
       .then(data => setRepositories(data));
   }, []);
 
-  console.log(repositories);
-
   return (
     <section className="repository-list">
         <h1>Lista de repositórios</h1>
 
         <ul>
-          {repositories.map((repository) => <RepositoryItem key={repository.id} repository={repository} />)}
+          {repositories.map((repository) => <RepositoryItem key={repository.name} repository={repository} />)}
         </ul>
     </section>
   )
